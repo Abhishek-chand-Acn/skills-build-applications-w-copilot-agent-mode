4e1b2a075b21ef483102a8cd13392740fcd606fe
Abhishek-chand-Acn <abhishek.a.chand@accenture.com>
2025-11-27 12:52:14 +0000

    Update ALLOWED_HOSTS configuration to support Codespaces

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-_xa5wtzz9c$%x191vb7x#pybajocptp!sq7h*j8a70s+6!17%&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
